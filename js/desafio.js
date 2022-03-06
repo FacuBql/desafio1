c81e599e2440908a18e0b7ed90fd4fe061e0ed40
FacuBql <facubaquela@gmail.com>
2022-03-06 02:48:43 -0300

    correciones 2

--- js/desafio.js
+++ js/desafio.js
@@ -7,24 +7,12 @@ const appointment = (dayEntry, monthEntry, yearEntry) => {
     return (
       "Faltan" +
       " " +
-      (day - dayEntry) +
+      (day - dayEntry + month - monthEntry + year - yearEntry) +
       " " +
-      "dias y" +
-      " " +
-      (month - monthEntry) +
-      " " +
-      "meses para el turno asignado."
+      "dias para el turno asignado."
     );
   } else if (scheduleToday(dayEntry, monthEntry, yearEntry)) {
     return "Tiene un turno asignado para el dia de hoy.";
-  } else {
-    return (
-      "Su ultimo turno fue asignado hace" +
-      " " +
-      (day + dayEntry + 20) +
-      " " +
-      "dias."
-    );
   }
 };
 
@@ -44,4 +32,4 @@ const timeUntilSchedule = (dayEntry, monthEntry) => {
   }
 };
 
-console.log(appointment(01, 04, 2022));
+console.log(appointment(01, 03, 2022));
