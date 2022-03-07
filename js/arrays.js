a58658cbc215c24bc6d61033c87d2d849c29e018
FacuBql <facubaquela@gmail.com>
2022-03-07 19:58:42 -0300

    Arrays&Objetos

---
+++ js/arrays.js
@@ -0,0 +1,10 @@
+const specialist = [
+  "Dr. Marcelo Perez",
+  "Neurologia",
+  "14.30 hs",
+  "Domingo 6/03/2022",
+];
+
+for (let i = 0; i < specialist.length; i++) {
+  console.log(specialist[i]);
+};
---
+++ js/objetos.js
@@ -0,0 +1,75 @@
+const coveragePlan1 = {
+  plan: "Umbrella Red",
+  coverage: "50%",
+  fee: "3.5k",
+  inscription: "4k",
+};
+const coveragePlan2 = {
+  plan: "Umbrella Blue",
+  coverage: "75%",
+  fee: "5k",
+  inscription: "4k",
+};
+const coveragePlan3 = {
+  plan: "Umbrella Gold",
+  coverage: "100%",
+  fee: "7k",
+  inscription: "4k",
+};
+
+console.log(
+  "Nuestro plan basico" +
+    " " +
+    coveragePlan1.plan +
+    " " +
+    "tiene un valor de inscripcion de" +
+    " " +
+    coveragePlan1.inscription +
+    ", con un valor mensual de" +
+    " " +
+    coveragePlan1.fee +
+    " " +
+    "y una cobertura del" +
+    " " +
+    coveragePlan1.coverage +
+    " " +
+    "para el afiliado."
+);
+
+console.log(
+  "Nuestro plan intermedio" +
+    " " +
+    coveragePlan2.plan +
+    " " +
+    "tiene un valor de inscripcion de" +
+    " " +
+    coveragePlan2.inscription +
+    ", con un valor mensual de" +
+    " " +
+    coveragePlan2.fee +
+    " " +
+    "y una cobertura del" +
+    " " +
+    coveragePlan2.coverage +
+    " " +
+    "para el afiliado."
+);
+
+console.log(
+  "Nuestro plan avanzado" +
+    " " +
+    coveragePlan3.plan +
+    " " +
+    "tiene un valor de inscripcion de" +
+    " " +
+    coveragePlan3.inscription +
+    ", con un valor mensual de" +
+    " " +
+    coveragePlan3.fee +
+    " " +
+    "y una cobertura del" +
+    " " +
+    coveragePlan3.coverage +
+    " " +
+    "para el afiliado."
+);
